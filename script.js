ef1828d6cb662d29445537ac4d4c57691be93e3b
Alisson Martin Lopes <103538181+AlissonMartin@users.noreply.github.com>
2022-07-03 14:18:03 -0300

    finalização da seção partner

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 totalSlides = document.querySelectorAll('.slider__item').length
 currentSlide = 0
-
+bdgImg = ''
 document.querySelector('.slider__width').style.width = `calc(100vw * ${totalSlides})`
 document.querySelector('.slider__controls').style.height = `${document.querySelector('.slider').clientHeight}px`
 
@@ -24,4 +24,24 @@ function updateMargin() {
     let sliderItemWidth = document.querySelector('.slider__item').clientWidth
     let newMargin = currentSlide * sliderItemWidth
     document.querySelector('.slider__width').style.marginLeft = `-${newMargin}px`
+}
+
+
+function hideTitle(e) {
+    var styles = window.getComputedStyle(e.target)
+    bgdImg = styles.backgroundImage
+    e.target.style.backgroundImage = 'none'
+    e.target.style.backgroundColor = '#171f36'
+    setTimeout(()=> {
+        e.target.style.backgroundColor = ' #222E50'
+    },500)
+    e.target.querySelector('.title').style.display = 'none'
+    e.target.querySelector('.subtitle').style.display = 'block'
+}
+
+function showTitle(e) {
+    e.target.style.backgroundImage = bgdImg
+    e.target.style.backgroundColor = 'none'
+    e.target.querySelector('.title').style.display = 'inline-block'
+    e.target.querySelector('.subtitle').style.display = 'none'
 }
